448cd8ab80bb6e6733643925066ff22bcdaee329
Ubuntu <ubuntu@ip-172-31-28-26.ec2.internal>
2019-10-29 18:11:50 +0000

    Initial commit

---
+++ helloworld.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2019 Evano Fotia All Rights reserved.
+//
+// Created by: Evano Fotia
+// Created on: Sep 2019
+// Reaching max students
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, world!" << std::endl;
+}
